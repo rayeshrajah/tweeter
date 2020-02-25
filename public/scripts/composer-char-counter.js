6ecbadafcf08cc20dbd342228bbd7cfa72bff939
Rayesh <rayeshrajah@gmail.com>
2020-02-25 23:10:30 +0000

    Completed new-tweeter css and using jquery

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  let textArea = $("textarea");
+  let maxLength = 140;
+  $(".counter").text(maxLength);
+  textArea.on("keypress", e => {
+    countChars = maxLength - textArea.val().length;
+    if (countChars < 0) {
+      $(".counter")
+        .text(countChars)
+        .css("color", "red");
+    } else if (countChars === 140) {
+      $(".counter").text(maxLength);
+    } else if (countChars < 140) {
+      $(".counter")
+        .text(countChars)
+        .css("color", "#545149");
+    }
+  });
+});
