d22afb8d1af85b21a13f183c95c8a3ac275ce187
Rayesh <rayeshrajah@gmail.com>
2020-02-26 21:32:08 +0000

    finished form handling and get and post request using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,66 +3,71 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+const server = "http://localhost:8080";
 
 //Creating an article for the tweets
-const createTweetElement = function(tweetData){
-    let $date = new Date(tweetData.created_at);
-    const $days = Math.floor($date / (20*60*60*1000));
-    const $tweet =(`<article class="tweet">` + 
-             `<header>` + 
-             `<p>` + 
-             `<img src="${tweetData.user.avatars}">` + 
-             `${tweetData.user.name}</p>` +
-             `<h6>${tweetData.user.handle}</h6>` +
-             `</header>` +
-             `<p>${tweetData.content.text}</p>` +
-             `<footer>` +
-             `<h6>${$days} days ago</h6>` +
-             `<div>` +
-             `<i class="fas fa-flag"></i>`    +
-             `<i class="fas fa-retweet"></i>` +
-             `<i class="fas fa-heart"></i>` +
-             `</div>` + 
-             `</footer>` + 
-             `</article>`);
+const createTweetElement = function(tweetData) {
+  let $date = new Date(tweetData.created_at);
+  const $days = Math.floor($date / (20 * 60 * 60 * 1000));
+  const $tweet =
+    `<article class="tweet">` +
+    `<header>` +
+    `<p>` +
+    `<img src="${tweetData.user.avatars}">` +
+    `${tweetData.user.name}</p>` +
+    `<h6>${tweetData.user.handle}</h6>` +
+    `</header>` +
+    `<p>${tweetData.content.text}</p>` +
+    `<footer>` +
+    `<h6>${$days} days ago</h6>` +
+    `<div>` +
+    `<i class="fas fa-flag"></i>` +
+    `<i class="fas fa-retweet"></i>` +
+    `<i class="fas fa-heart"></i>` +
+    `</div>` +
+    `</footer>` +
+    `</article>`;
 
-    return $tweet;
-}
-//looping over the dynamic database, using the createTweet 
+  return $tweet;
+};
+//looping over the dynamic database, using the createTweet
 //for each object and appending those object to the section tag with id: #tweets-container
-const renderTweets = function(tweets){
-    for(let i = 0; i < tweets.length; i++){
-        $tweet = createTweetElement(tweets[i]);
-        $('#tweets-container').append($tweet);
-    }
-}
-//Renders function that renders the tweets to the page.
-$(document).ready(function() {
-    renderTweets(data);
+const renderTweets = function(tweets) {
+  for (let i = 0; i < tweets.length; i++) {
+    $tweet = createTweetElement(tweets[i]);
+    $("#tweets-container").append($tweet);
+  }
+};
+
+$(".tweet-form").on("submit", function(event) {
+  // const formHandler = function(){
+  const MAXLENGTH = 140;
+  let $isTextAreaEmpty = $("textarea").val();
+
+  if (MAXLENGTH <= $("textarea").val().length) {
+    alert("You cannot tweet because you went over the amount of letters!");
+    return;
+  } else if ($isTextAreaEmpty === "") {
+    alert("You have entered nothing");
+    return;
+  }
+  event.preventDefault();
+  $.ajax({
+    method: "POST",
+    url: `${server}/tweets`,
+    data: $(".tweet-form").serialize()
+  });
 });
 
+const loadTweets = function() {
+    $.ajax({
+    method: "GET",
+    url: `/tweets`,
+    success: renderTweets
+  });
+};
+
+loadTweets();
+
+//Renders function that renders the tweets to the page.
+//renderTweets(data);
